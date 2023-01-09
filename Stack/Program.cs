c3205b41bf92d84e4e2d197063225d97f9c10d73
‪ahmad rahhal‬‏ <ahmad.2001.rahhal@gmail.com>
2023-01-09 17:32:11 +0300

    Initial commit

---
+++ Stack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>(5);
+            stack.push("this is first item");
+            stack.push("this is 2 item");
+            stack.push("this is 3 item");
+            stack.push("this is 4 item");
+            stack.push("this is 5 item");
+            stack.push("this is 5 item");
+            stack.pup();
+            stack.pup();
+            stack.pup();
+            stack.pup();
+            stack.pup();
+
+            stack.test();
+            Console.WriteLine(stack.isempty());
+            Console.WriteLine(stack.isfull());
+
+        }
+        
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack<T>
+    {
+        int size;
+        object[] ArrayStack;
+        int top;
+        public Stack(int size)
+        {
+            this.size = size;
+            ArrayStack = new object[this.size];
+            this.top = -1;
+           
+        }
+
+        public void push(object newItem)//this mithod for addElmante to stack
+        {
+            if (isfull())
+            {
+                Console.WriteLine(" ERROR!! \n You can not add any element; the stack is full, \n the elemnt did not added to stack");
+            }
+            else { 
+            top ++;
+            ArrayStack[top]= newItem;
+            }
+        }
+
+        public void pup()//this mithod for addElmante to stack
+        {
+            if (isempty())
+            {
+                Console.WriteLine(" ERROR!! \n the stack is empty");
+            }
+            else
+            {
+                ArrayStack[top] = "";
+                top--;
+            }
+           
+
+        }
+
+        public bool isfull()
+        {
+            return (top == size-1);
+        }
+
+        public bool isempty()
+        {
+            return (top == -1);
+        }
+
+        public void test()
+        {
+            if (isempty())
+            {
+                Console.WriteLine(" ERROR!! \n the stack is empty");
+            }
+            else
+            {
+                Console.WriteLine(this.ArrayStack[top]);
+            }
+            
+        }
+    }
+}
